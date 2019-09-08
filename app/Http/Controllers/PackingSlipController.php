ec01354c3bc95feeb0fafbe2a4629cb82fe71db5
Trystan Hendricks <dolkhound@gmx.com>
2019-09-07 20:59:59 -0400

    PDF created Footer partially create

--- app/Http/Controllers/PackingSlipController.php
+++ app/Http/Controllers/PackingSlipController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PDF;
 
 class PackingSlipController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('PDFTest');
+    }
+ 
+
+    public function simplePDF()
+    {
+        PDF::SetTitle('Testing PDF');
+        PDF::AddPage('L', 'letter');
+        PDF::SetFont('courier', 15);
+
+        PDF::Text(32,10,'Packing Slip');
+        //Footer
+        PDF::Footer(
+            PDF::Text(270,175,'Page '. PDF::getAliasNumPage(). '/'. PDF::getAliasNbPages())
+        );
+        PDF::AddPage('L', 'letter');
+        
+        PDF::Output('testing.pdf');
+    }
 }
