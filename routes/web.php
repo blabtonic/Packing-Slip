0f2de9c655300011c36a2ed13daeaf0f31cb5045
Trystan Hendricks <dolkhound@gmx.com>
2019-09-07 20:58:43 -0400

    created routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/PDFTest', ['as' => 'PDFTest', 'uses' => 'PackingSlipController@index']);
+Route::get('/simple-pdf', ['as' => 'SimplePDF', 'uses' => 'PackingSlipController@simplePDF']);
